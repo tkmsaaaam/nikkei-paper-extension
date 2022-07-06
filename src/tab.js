d88c3d92d36ceae61da23d0025118d47928e8558
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-06 22:44:27 +0900

    add src/tab.js

---
+++ src/tab.js
@@ -0,0 +1,9 @@
+document.addEventListener('click', function (e) {
+	e.preventDefault();
+	rawHref = e.target.href;
+	host = 'https://www.nikkei.com/';
+	path = rawHref.substr(rawHref.indexOf('/paper/article/'));
+	chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+		chrome.tabs.update(tabs[0].id, { url: host + path });
+	});
+});
