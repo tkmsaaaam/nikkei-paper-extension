4850614e7df57729dc13a27365e2c095eda4f552
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-09 13:35:17 +0900

    separate method in src/tab.js

--- src/tab.js
+++ src/tab.js
@@ -1,9 +1,15 @@
-document.addEventListener('click', e => {
-	e.preventDefault();
-	rawHref = e.target.href;
-	host = 'https://www.nikkei.com/';
-	path = rawHref.substr(rawHref.indexOf('/paper/article/'));
+(() => {
+	document.addEventListener('click', e => {
+		e.preventDefault();
+		const rawHref = e.target.href;
+		transition(rawHref);
+	});
+})();
+
+const transition = rawHref => {
+	const host = 'https://www.nikkei.com/';
+	const path = rawHref.substr(rawHref.indexOf('/paper/article/'));
 	chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
 		chrome.tabs.update(tabs[0].id, { url: host + path });
 	});
-});
+};
